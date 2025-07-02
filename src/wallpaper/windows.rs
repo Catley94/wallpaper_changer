b2ba1122d753588e4cea8d5604f409048644a651
positive-season <scp94373@gmail.com>
2025-07-02 21:14:57 +0100

    v0.2.2 Fixing Windows wallpaper bug
    Sometimes wallpaper didn't change in Windows.

--- src/wallpaper/windows.rs
+++ src/wallpaper/windows.rs
@@ -10,17 +10,34 @@ pub fn explorer(path: &str) -> std::io::Result<std::process::Output> {
     }
 
     println!("Windows: Setting wallpaper to {}", path);
-    // Using PowerShell to set wallpaper on Windows
-    Command::new("powershell")
-        .args([
-            "-command",
-            "Set-ItemProperty -path 'HKCU:\\Control Panel\\Desktop\\' -name Wallpaper -value",
-            path,
-            ";",
-            "RUNDLL32.EXE",
-            "user32.dll,UpdatePerUserSystemParameters",
-            ",1",
-            ",True",
-        ])
-        .output()
+
+    // Create and execute the PowerShell script with the more reliable method
+    let ps_script = format!(
+        "Add-Type @\"
+using System;
+using System.Runtime.InteropServices;
+public class Wallpaper {{
+    [DllImport(\"user32.dll\", CharSet = CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}}
+\"@;
+[Wallpaper]::SystemParametersInfo(0x0014, 0, '{}', 0x01 -bor 0x02)",
+        path.replace("'", "''") // Escape single quotes for PowerShell
+    );
+
+    let output = Command::new("powershell")
+        .args(["-command", &ps_script])
+        .output()?;
+
+    // Print the command output
+    if output.status.success() {
+        println!("Wallpaper set successfully");
+    } else {
+        if let Ok(stderr) = String::from_utf8(output.clone().stderr) {
+            eprintln!("Error setting wallpaper: {}", stderr);
+        }
+        eprintln!("Command failed with status: {}", output.status);
+    }
+
+    Ok(output)
 }
